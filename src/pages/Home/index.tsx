fa55c70a7be4b87ae8983cc16c24c26d645ef779
Leandro Ribeiro Moura <leandro.rmoura@b2wdigital.com>
2022-11-04 20:55:07 -0300

    Alterações em home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,21 +1,36 @@
 import photoAssets from './photo.json';
 import Footer from '../../components/Footer';
 import style from './Home.module.scss';
+import { Divider, Typography } from '@mui/material';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 export default function index() {
   const photos = photoAssets;
-
+  const matches = useMediaQuery('(max-width:1000px)');
   return (
-    <section className={style.section__perfil}>
-      <img className={style.photo__perfil} src={photos[0].src} />
-      <p className={style.text__perfil}>
-        Olá, sou Lucas Shtorache fotógrafo e videomaker localizado em Santos/SP.
-        Atuo na area desde 2018 e meu foco é ajudar artistas e marcas a criarem
-        sua identidade visual através de fotos e videos.
-      </p>
+    <>
+      <section >
+        <div className={style.section__perfil}>
+          <img className={style.photo__perfil} src={photos[0].src} />
+          <Divider orientation={matches ? 'horizontal' : 'vertical'} variant="middle" flexItem sx={{margin: '24px'}}/>
+          <div className={style.text__wrapper}>
+            <Typography variant="body1" gutterBottom>
+              Me chamo Lucas Shtorache sou fotográfo e videomaker baseado em
+              Santos/SP.
+            </Typography>
+            <Typography variant="body1" gutterBottom>
+              Já trabalhei com diversos artistas como: Bayside Kings, Gloria, Black Pantera, Sepultura, Ghostmane, While She Sleeps, Surra, Bullet Bane dentre outros do cenário nacional e internacional.
+            </Typography>
+            <Typography variant="body1" gutterBottom>
+              Trabalho não apenas na area da musicas mas também em outros seguimentos,
+              como gastronomia e marcas.
+            </Typography>
+          </div>
+        </div>
+      </section>
       <div className={style.footer__section}>
         <Footer />
       </div>
-    </section>
+    </>
   );
 }
